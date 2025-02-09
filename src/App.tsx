da3262bbc33f2767ca95923eab34e8a675da7f5f
m-essam-s <sir.m.essam.g@gmail.com>
2025-02-09 02:00:28 +0200

    feat: Keyboard functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from "react"
 import Header from './components/Header'
 import GameStatuse from './components/GameStatuse'
 import Keyboard from './components/Keyboard'
@@ -7,14 +8,28 @@ import Word from './components/Word'
 import NewGameButton from './components/NewGameButton'
 
 function App() {
+  const [currentWord, setCurrentWord] = useState<string>("react")
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const addGuessedLetter = (letter: string) => {
+    setGuessedLetters(
+      prevLetters => prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    )
+  }
+
+  console.log(guessedLetters)
 
   return (
     <main className='flex flex-col items-center '>
       <Header />
       <GameStatuse />
       <LanguageChips />
-      <Word currentWord={"react"} />
-      <Keyboard />
+      <Word currentWord={currentWord} />
+      <Keyboard
+        addGuessedLetter={addGuessedLetter}
+        currentWord={currentWord}
+        guessedLetters={guessedLetters}
+      />
       <NewGameButton />
     </main>
   )
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,14 +1,36 @@
-const Keyboard = () => {
+import clsx from "clsx"
+
+const Keyboard = (
+    {
+        addGuessedLetter,
+        currentWord,
+        guessedLetters
+    }: {
+        addGuessedLetter: (arg0: string) => void,
+        currentWord: string,
+        guessedLetters: string[]
+    }
+) => {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return (
         <section className="flex flex-wrap justify-center gap-[8px] max-w-[450px] m-[36px]">
-            {alphabet.split("").map(letter => (
-                <button
-                    key={letter}
-                    className="rounded-[3px] bg-[#FCBA29] text-[#000] border-[1px] border-[#D7D7D7] w-[35px] h-[35px] cursor-pointer"
-                >
-                    {letter.toUpperCase()}</button>
-            ))}
+            {alphabet.split("").map(letter => {
+                const isGuessed = guessedLetters.includes(letter)
+                const isCorrect = isGuessed && currentWord.split("").includes(letter)
+                const isWrong = isGuessed && !currentWord.split("").includes(letter)
+                return (
+                    <button
+                        key={letter}
+                        onClick={() => addGuessedLetter(letter)}
+                        className={clsx(
+                            "rounded-[3px] bg-[#FCBA29] text-[#000] border-[1px] border-[#D7D7D7] w-[35px] h-[35px] cursor-pointer",
+                            isCorrect && "bg-green-500",
+                            isWrong && "bg-red-500"
+                        )}
+                    >
+                        {letter.toUpperCase()}</button>
+                )
+            })}
         </section>
     );
 }
